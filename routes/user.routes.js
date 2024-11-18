2b239f2c694ab318704887c4ef85eb185120105f
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-11-19 02:17:56 +0530

    developed the routes and api-end points for the user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,25 @@
-import express from "express";
-const router = express.Router();
-
-
-
+import { Router } from "express";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
+import {
+  signUpUser,
+  loginUser,
+  logoutUser,
+  getUserId,
+  updateUserId,
+  refreshAccessToken,
+} from "../controllers/user.controllers.js";
 
+const router = Router();
 
+router.route("/sign-up").post(signUpUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(isLoggedIn, logoutUser);
+
+// getting user by id
+router.route("/u/:user_id").get(getUserId);
+// updating the user by id
+router.route("/update/:user_id").put(isLoggedIn, updateUserId);
+// refresh access token
+router.route("/token/refresh").post(refreshAccessToken);
 
 export default router;
