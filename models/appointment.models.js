462f599f1b490d4d909d0144e8e580a4c90b791e
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-11-18 18:23:00 +0530

    developed the appointment model

---
+++ models/appointment.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    // appointment date, description, status, vet_Info, client_Info
+    appointment_Date: {
+      type: Date,
+      required: [true, "Please enter the date of appointment"],
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["None","Pending", "Scheduled", "Completed", "Cancelled"],
+      default: "None"
+    },
+    vet_Info: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Vet information is required"],
+    },
+    client_info: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Client information is required"],
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_At",
+      updatedAt: "updated_At",
+    },
+  }
+);
+
+const appointmentModel = mongoose.model("Appointment", appointmentSchema);
+export default appointmentModel;
