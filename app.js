cb6599a33c970ea25bbe0ac2f9b29e52fcdd76e0
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-11-19 01:56:06 +0530

    defined the apis

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv/config";
 import connectDB from "./config/db.js";
-import indexRouter from "./routes/index.routes.js"
+import cookieParser from "cookie-parser";
+
 import userRouter from "./routes/user.routes.js";
+import productRouter from "./routes/product.routes.js";
+import petRouter from "./routes/pet.routes.js";
+import orderRouter from "./routes/order.routes.js";
+import appointmentRouter from "./routes/appointment.routes.js";
 
 connectDB();
 
@@ -10,11 +15,16 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", indexRouter);
-app.use("/users", userRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/pets", petRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/appointments", appointmentRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
