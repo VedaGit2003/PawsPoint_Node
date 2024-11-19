812fa388bc05ed50062a3a0d4de39f195400e11e
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-11-19 19:56:28 +0530

    routes phase 1 done

--- routes/pet.routes.js
+++ routes/pet.routes.js
@@ -1,37 +1,29 @@
 import { Router } from "express";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
-import { 
-    getAllPets,
-    createPets,
-    getPet,
-    updatePet,
-    deletePet,
-    searchPets,
-    getPetsBySellerId
- } from "../controllers/pet.controllers.js";
+import {
+  getAllPets,
+  createPets,
+  getPet,
+  updatePet,
+  deletePet,
+  searchPets,
+  getPetsBySellerId,
+} from "../controllers/pet.controllers.js";
 
 const router = Router();
 
-router
-.route("/")
-.get(getAllPets)
-.post(isLoggedIn, createPets);
+router.route("/").get(getAllPets).post(isLoggedIn, createPets);
 
 router
-.route("/p/:pet_id")
-.get(getPet)
-.put(isLoggedIn, updatePet)
-.delete(isLoggedIn, deletePet);
+  .route("/p/:pet_id")
+  .get(getPet)
+  .put(isLoggedIn, updatePet)
+  .delete(isLoggedIn, deletePet);
 
 // search pets by keywords
-router
-.route("/search")
-.get(searchPets);
+router.route("/search").get(searchPets);
 
 // search pets by seller id
-router
-.route("/seller/:sellerId")
-.get(getPetsBySellerId);
-
+router.route("/seller/:sellerId").get(getPetsBySellerId);
 
 export default router;
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,42 +1,33 @@
 import { Router } from "express";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
-import { 
-    getAllProducts,
-    createProducts,
-    getProduct,
-    updateProduct,
-    deleteProduct,
-    searchProduct,
-    getProductsBySellerId,
-    getProductsBySellerName
- } from "../controllers/pet.controllers.js";
+import {
+  getAllProducts,
+  createProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+  searchProduct,
+  getProductsBySellerId,
+  getProductsBySellerName,
+} from "../controllers/product.controllers.js";
 
 const router = Router();
 
-router
-.route("/")
-.get(getAllProducts)
-.post(isLoggedIn, createProducts);
+router.route("/").get(getAllProducts).post(isLoggedIn, createProducts);
 
 router
-.route("/p/:product_id")
-.get(getProduct)
-.put(isLoggedIn, updateProduct)
-.delete(isLoggedIn, deleteProduct);
+  .route("/p/:product_id")
+  .get(getProduct)
+  .put(isLoggedIn, updateProduct)
+  .delete(isLoggedIn, deleteProduct);
 
 // search product by keywords
-router
-.route("/search")
-.get(searchProduct);
+router.route("/search").get(searchProduct);
 
 // search product by seller id
-router
-.route("/seller/:sellerId")
-.get(getProductsBySellerId);
+router.route("/seller/:sellerId").get(getProductsBySellerId);
 
 // search product by seller name
-router
-.route("/seller/:username")
-.get(getProductsBySellerName);
+router.route("/seller/:username").get(getProductsBySellerName);
 
 export default router;
