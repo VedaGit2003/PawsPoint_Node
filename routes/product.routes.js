d408b75d11eaf6c1424786d24a9898cd36e01e69
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-11-19 13:41:16 +0530

    developed the routes and api-end points for the products

---
+++ routes/product.routes.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { isLoggedIn } from "../middlewares/isLoggedIn.js";
+import { 
+    getAllProducts,
+    createProducts,
+    getProduct,
+    updateProduct,
+    deleteProduct,
+    searchProduct,
+    getProductsBySellerId,
+    getProductsBySellerName
+ } from "../controllers/pet.controllers.js";
+
+const router = Router();
+
+router
+.route("/")
+.get(getAllProducts)
+.post(isLoggedIn, createProducts);
+
+router
+.route("/p/:product_id")
+.get(getProduct)
+.put(isLoggedIn, updateProduct)
+.delete(isLoggedIn, deleteProduct);
+
+// search product by keywords
+router
+.route("/search")
+.get(searchProduct);
+
+// search product by seller id
+router
+.route("/seller/:sellerId")
+.get(getProductsBySellerId);
+
+// search product by seller name
+router
+.route("/seller/:username")
+.get(getProductsBySellerName);
+
+export default router;
