b7367263f7726869eb2f4766d59850b101ab441c
varunverma1303@gmail.com <varunverma1303@gmail.com>
2024-11-21 20:02:12 +0530

    wrote the business logic for isLoggedIn middleware

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -2,7 +2,22 @@ import userModel from "../models/user.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
-const isLoggedIn = asyncHandler();
+const isLoggedIn = asyncHandler(async (req, res, next) => {
+    const token = req.cookies.token;
+    if(!token) {
+        throw new ApiError(400, "User is not logged in");
+    }
+
+    let decode = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await userModel.findById({ _id: decode.id }).select(" -password");
+
+    if(!user) {
+        throw new ApiError(400, "User does not exist");
+    }
+    req.user = user;
+    next();
+});
 
 export { isLoggedIn };
