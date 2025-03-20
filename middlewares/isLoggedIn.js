5a1a6a7869c01349e43d28f0b829ac51af591a0c
VedaGit2003 <vedapaulchowdhury@gmail.com>
2025-03-20 13:17:06 +0530

    changes in controller and addition of  payment.controller.js

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -6,6 +6,7 @@ import jwt from "jsonwebtoken";
 
 const isLoggedIn = asyncHandler(async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  console.log(token)
   if (!token) {
     return res
       .status(400)
